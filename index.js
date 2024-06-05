4590dc15371e5508eabd7914216324071e5e2a80
Galaxyjunction <kehkashan.sidd@gmail.com>
2024-06-05 14:26:01 -0700

    Adding a word counter project.

---
+++ index.js
@@ -0,0 +1,9 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let user_answer = await inquirer.prompt({
+    type: "input",
+    name: "words",
+    message: "Enter your sentence.",
+});
+let word_count = user_answer.words.trim().split(" ").length;
+console.log(`Your sentence has ${word_count} words.`);
